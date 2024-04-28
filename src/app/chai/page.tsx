d33973f2a80166b3df691168da3a3f38a5540164
Ubais88 <moubais2143@gmail.com>
2024-04-28 15:16:03 +0530

    navbar added

--- src/app/chai/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const chaiPage = () => {
-  return (
-    <div className='text-4xl'>ChaiPage 1</div>
-  )
-}
-
-export default chaiPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='text-4xl'>Ubais 1st NExt App</div>
+    <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
+      <div className='text-4xl text-center'>Ubais 1st NExt App</div>
+    </main>
   )
 }
 
