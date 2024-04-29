d15005958399101d6d3ab38182e06c048b177a55
Ubais88 <moubais2143@gmail.com>
2024-04-29 15:05:35 +0530

    hero Section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import HeroSection from '@/components/HeroSection'
 import React from 'react'
 
 const page = () => {
   return (
     <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
-      <div className='text-4xl text-center'>Ubais 1st NExt App</div>
+      <HeroSection/>
     </main>
   )
 }
 
-export default page
+export default page 
