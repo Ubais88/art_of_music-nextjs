2c9049ae1be8f480a4c0f350b5725f9550a1e299
Ubais88 <moubais2143@gmail.com>
2024-04-29 15:46:45 +0530

    courses cards added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from '@/components/FeaturedCourses'
 import HeroSection from '@/components/HeroSection'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const page = () => {
   return (
     <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
       <HeroSection/>
+      <FeaturedCourses/>
     </main>
   )
 }
