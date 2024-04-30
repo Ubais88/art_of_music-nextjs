db805c8f0ca8a096529a51736a53b73bbc9d3b05
Ubais88 <moubais2143@gmail.com>
2024-04-30 07:38:18 +0530

    why choose us section  added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import FeaturedCourses from '@/components/FeaturedCourses'
 import HeroSection from '@/components/HeroSection'
+import WhyChooseUs from '@/components/WhyChooseUs'
 import React from 'react'
 
 const page = () => {
   return (
-    <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]'>
+    <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] '>
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUs/>
     </main>
   )
 }
