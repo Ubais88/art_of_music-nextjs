aedc0c666beca789d4c1b38b3184523fe836f9d8
Ubais88 <moubais2143@gmail.com>
2024-05-05 11:12:37 +0530

    course page added

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Image from "next/image";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div>Contact Page</div>
+  )
+}
+
+export default page
